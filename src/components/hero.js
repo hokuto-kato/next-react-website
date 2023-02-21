eb102ecc729019b13b92d5d5067ca83b10dde45b
hokuto-kato <hokuto15@gmail.com>
2023-02-21 21:00:59 +0900

    [3.5]ヒーローのテキストのスタイルを設定する

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,9 +1,24 @@
+import { css } from '@emotion/react'
+
 export const Hero = ({ title, subtitle, imageOn = false }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>{subtitle}</p>
+    <div css={text}>
+      <h1 css={heading}>{title}</h1>
+      <p css={subHeading}>{subtitle}</p>
       {imageOn && <figure>[画像]</figure>}
     </div>
   )
 }
+
+const text = css`
+	padding-top: calc(var(--display) * 0.5);
+	padding-bottom: calc(var(--display) * 0.7);
+`
+const heading = css`
+	font-size: var(--display);
+	font-weight: 900;
+	letter-spacing: 0.15em;
+`
+const subHeading = css`
+	font-size: var(--small-heading2);
+`
