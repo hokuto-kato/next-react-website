9afe0e14e1f0e43c40c123a2f3ce035b4e143c31
hokuto-kato <hokuto15@gmail.com>
2023-03-08 20:51:10 +0900

    fix fontawesome-svg-coreのimportが重複していたのと、importはすべてまとめて一番上に書かないとbuild出来なかったので修正

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,11 @@
 import '@/styles/global.css'
 import { Layout } from '@/components/layout'
-import { config } from '@fortawesome/fontawesome-svg-core'
+import { config, library } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
-
-import { library } from '@fortawesome/fontawesome-svg-core'
 import { faHouseChimney } from '@fortawesome/free-solid-svg-icons'
 import { faSun } from '@fortawesome/free-regular-svg-icons'
 import { faTwitter, faFacebookF } from '@fortawesome/free-brands-svg-icons'
+config.autoAddCss = false
 library.add(faHouseChimney, faSun, faTwitter, faFacebookF)
 
 export default function App({ Component, pageProps }) {
