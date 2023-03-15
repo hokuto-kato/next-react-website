20e9ec7b8220de024675088b3ef6cd74e1711058
hokuto-kato <hokuto15@gmail.com>
2023-03-15 20:36:13 +0900

    [6.6]カスタムDocumentコンポーネントで<html>にlangを入れる まで

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import { Hero } from '@/components/hero'
 import { Container } from '@/components/container'
+import { Meta } from '@/components/meta'
 
 export default function Home() {
   return (
     <Container>
+      <Meta pageDesc="アウトプットしていくサイト" />
       <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn />
     </Container>
   )
